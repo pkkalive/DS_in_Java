ab22747b05a778c61841094074df57b992a80a4d
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2023-10-03 20:24:43 +0530

    A_Says_Hi_To_B

---
+++ src/Scaler/A_Says_Hi_To_B.java
@@ -0,0 +1,12 @@
+package Scaler;
+
+import java.util.Scanner;
+
+public class A_Says_Hi_To_B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.nextLine();
+        String B = sc.nextLine();
+        System.out.println(A +" says Hi to " +B);
+    }
+}
