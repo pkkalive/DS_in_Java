8d7e9c444fa720c11b35a9f9ae03837c3af8a896
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2023-10-03 20:20:19 +0530

    PrintABCDE

---
+++ src/Scaler/PrintABCDE.java
@@ -0,0 +1,11 @@
+package Scaler;
+
+public class PrintABCDE {
+    public static void main(String[] args) {
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("D");
+        System.out.println("E");
+    }
+}
