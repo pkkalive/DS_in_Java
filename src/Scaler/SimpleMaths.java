6c436f85f5501566eb03074893920027e1c6c443
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2023-10-14 16:38:53 +0530

    SimpleMaths

---
+++ src/Scaler/SimpleMaths.java
@@ -0,0 +1,16 @@
+package Scaler;
+
+import java.util.Scanner;
+
+public class SimpleMaths {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println( A + B);
+        System.out.println( A - B);
+        System.out.println( A * B);
+        System.out.println( A / B);
+    }
+}
