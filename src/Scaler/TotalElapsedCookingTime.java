1b7bd73e08cc4304803b003032e7ff101ffad969
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2023-10-14 18:50:39 +0530

    TotalElapsedCookingTime

---
+++ src/Scaler/TotalElapsedCookingTime.java
@@ -0,0 +1,12 @@
+package Scaler;
+
+import java.util.Scanner;
+
+public class TotalElapsedCookingTime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        System.out.println((2 * N) + M);
+    }
+}
