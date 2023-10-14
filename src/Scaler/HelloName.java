baeda3ed02050d152ca6e8b27a4e7b037caff73c
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2023-10-14 17:30:16 +0530

    HelloName

---
+++ src/Scaler/HelloName.java
@@ -0,0 +1,10 @@
+package Scaler;
+
+import java.util.Scanner;
+
+public class HelloName {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello " + sc.next());
+    }
+}
