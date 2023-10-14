7e0407bbac73398be426a8b7b59d2c61a41f7e97
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2023-10-14 17:52:20 +0530

    PrintSentence

---
+++ src/Scaler/PrintSentence.java
@@ -0,0 +1,10 @@
+package Scaler;
+
+import java.util.Scanner;
+
+public class PrintSentence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine());
+    }
+}
