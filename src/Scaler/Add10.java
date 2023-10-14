6ff724b08fdb33a516f6cea7ba241e17c867ee0b
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2023-10-14 17:04:09 +0530

    Add10

---
+++ src/Scaler/Add10.java
@@ -0,0 +1,10 @@
+package Scaler;
+
+import java.util.Scanner;
+
+public class Add10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + 10);
+    }
+}
