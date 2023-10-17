175b8e89a6aab39a32b68972517d74ebee21f4f6
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2023-10-18 02:19:13 +0530

    LCMEasy

--- src/Scaler/LCMEasy.java
+++ src/Scaler/LCMEasy.java
@@ -1,7 +1,23 @@
 package Scaler;
 
+import java.util.Scanner;
+
 public class LCMEasy {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(lcm(A, B));
+    }
 
+    public static int gcd (int a, int b){
+        if (b == 0){
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b){
+        return (a * b)/gcd(a, b);
     }
 }
