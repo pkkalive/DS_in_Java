5d6a1409f9e1e367e2b88a07cfe9058145ca7004
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2023-10-20 23:53:30 +0530

    MakeIt

---
+++ src/Scaler/MakeIt.java
@@ -0,0 +1,12 @@
+package Scaler;
+
+import java.util.Scanner;
+
+public class MakeIt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(((A * 3) + B)/2);
+    }
+}
