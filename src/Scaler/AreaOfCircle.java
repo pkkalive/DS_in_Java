743dd4dc6976153cd71f8da441197872625b1524
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2023-10-21 00:02:58 +0530

    AreaOfCircle

--- src/Scaler/AreaOfCircle.java
+++ src/Scaler/AreaOfCircle.java
@@ -6,7 +6,6 @@ public class AreaOfCircle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
-        double area = 3.1416 * A * A;
-        System.out.format("%.2f", area);
+        System.out.format("%.2f", 3.1416 * A * A);
     }
 }
