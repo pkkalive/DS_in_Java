91c3f8871866a07f3480832125285bda7c8129d2
pkkalive <kalivepurushothamkumar@gmail.com>
2023-10-31 20:46:24 +0530

    PhotoFramePattern

--- src/Scaler/PhotoFramePattern.java
+++ src/Scaler/PhotoFramePattern.java
@@ -12,9 +12,7 @@ public class PhotoFramePattern {
     public static void  printPhotoFramePattern(int n){
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 || i == n - 1){
-                    System.out.print("*");
-                } else if (j == 0 || j == n - 1){
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1){
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
