b2e7b49e91e14513541ebec1330bc45821910e45
pkkalive <kalivepurushothamkumar@gmail.com>
2023-11-01 21:49:33 +0530

    Pattern8

---
+++ src/Strivers/Pattern8.java
@@ -0,0 +1,23 @@
+package Strivers;
+
+import java.util.Scanner;
+
+public class Pattern8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        printPattern8(N);
+    }
+    
+    public static void printPattern8(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
