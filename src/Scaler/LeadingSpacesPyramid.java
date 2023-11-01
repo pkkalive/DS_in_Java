83e77a2ac3b3f344bba5a8c47bd9209877e7da5c
pkkalive <kalivepurushothamkumar@gmail.com>
2023-11-01 21:13:08 +0530

    LeadingSpacesPyramid

--- src/Scaler/LeadingSpacesPyramid.java
+++ src/Scaler/LeadingSpacesPyramid.java
@@ -7,13 +7,12 @@ public class LeadingSpacesPyramid {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        for (int i = 1; i <= N ; i++) {
-            for (int j = N; j >= 1; j--) {
-                if (j > i){
-                    System.out.print(" ");
-                } else {
-                    System.out.print("*");
-                }
+        for (int i = 0; i < N ; i++) {
+            for (int j = 0; j < N - i -1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
