0954c67c4a280246a1ecd90ad53abee4c7828664
pkkalive <kalivepurushothamkumar@gmail.com>
2023-11-01 22:23:00 +0530

    Pattern9

--- src/Strivers/Pattern9.java
+++ src/Strivers/Pattern9.java
@@ -5,9 +5,30 @@ import java.util.Scanner;
 public class Pattern9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        int N = sc.nextInt();
-        printPattern9(5);
+        int N = sc.nextInt();
+        printPattern9(N);
     }
     public static void printPattern9(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (2 * i) + 1; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (2 * n) - ((2 * i) + 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
