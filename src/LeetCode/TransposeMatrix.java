b652b1af29e921ec2307b78327fd3d4aa2117c73
pkkalive <kalivepurushothamkumar@gmail.com>
2023-12-12 08:26:41 +0530

    TransposeMatrix

--- src/LeetCode/TransposeMatrix.java
+++ src/LeetCode/TransposeMatrix.java
@@ -15,9 +15,9 @@ public class TransposeMatrix {
 
     public static void main(String[] args) {
         int[][] ans = transpose(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
-        for (int i = 0; i < ans.length; i++) {
+        for (int[] an : ans) {
             for (int j = 0; j < ans[0].length; j++) {
-                System.out.print(ans[i][j] + " ");
+                System.out.print(an[j] + " ");
             }
             System.out.println();
         }
