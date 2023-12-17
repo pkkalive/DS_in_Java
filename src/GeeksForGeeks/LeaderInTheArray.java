6bcfebc5ddb40a7b6e4b279cf2f91e5cc540b04b
pkkalive <kalivepurushothamkumar@gmail.com>
2023-12-17 13:28:29 +0530

    LeaderInTheArray

--- src/GeeksForGeeks/LeaderInTheArray.java
+++ src/GeeksForGeeks/LeaderInTheArray.java
@@ -3,9 +3,10 @@ package GeeksForGeeks;
 public class LeaderInTheArray {
     static void leader(int[] arr){
         int n = arr.length - 1;
+        if (n <= 0) return;
         int curr_leader = arr[n];
         System.out.print(curr_leader + " ");
-        for (int i = n - 1; i >= 0 ; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             if (arr[i] > curr_leader){
                 System.out.print(arr[i] + " ");
                 curr_leader = arr[i];
@@ -19,5 +20,6 @@ public class LeaderInTheArray {
         leader(new int[]{7, 10, 4, 10, 6, 5, 2});
         leader(new int[]{10, 20, 30});
         leader(new int[]{30, 20, 10});
+        leader(new int[]{});
     }
 }
