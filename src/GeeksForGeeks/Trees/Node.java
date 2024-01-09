43860ae711bc7537656f6bf02b146bcfc2ec0994
pkkalive <kalivepurushothamkumar@gmail.com>
2024-01-09 05:35:38 +0530

    PreorderTraversal

---
+++ src/GeeksForGeeks/Trees/Node.java
@@ -0,0 +1,10 @@
+package GeeksForGeeks.Trees;
+
+public class Node {
+    public int data;
+    public Node left, right;
+     public Node (int d){
+         data = d;
+         left = right = null;
+     }
+}
---
+++ src/GeeksForGeeks/Trees/PreorderTraversal.java
@@ -0,0 +1,39 @@
+package GeeksForGeeks.Trees;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class PreorderTraversal {
+    private static ArrayList<Integer> preorder(Node root){
+        ArrayList<Integer> list = new ArrayList<>();
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        Node current = root;
+        while (current != null && !stack.empty()){
+            current = stack.pop();
+            list.add(current.data);
+            if (current.right != null)
+                stack.push(current.right);
+            if (current.left != null)
+                stack.push(current.left);
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.left.right.left = new Node(8);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        root.right.right.left = new Node(9);
+        root.right.right.right = new Node(10);
+
+        ArrayList<Integer> list = preorder(root);
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+        }
+    }
+}
